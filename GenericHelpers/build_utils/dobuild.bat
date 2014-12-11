e79562fb9981f10bf798032bc3b022cb12d465ed
edr <fuffa@deltatre.com>
2014-12-11 12:38:32 +0100

    Build process refactored

--- GenericHelpers/build_utils/dobuild.bat
+++ GenericHelpers/build_utils/dobuild.bat
@@ -1,8 +1,9 @@
-@echo off
+@echo off >NUL 2>NUL
 call dobuild_env.bat
 
 call dobuild_single GenericHelpers 4.0 net40
 call dobuild_single GenericHelpers 4.5 net45
 call dobuild_nuget GenericHelpers
 
+call dobuild_clean
 pause
